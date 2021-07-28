a47d9498e9b92d05b96284fccf87b9a0ffaf1725
Duong <duongp@wit.edu>
2021-07-28 11:43:38 -0400

    update gitignore to ignore images

--- parking_traker_website/tracker/views.py
+++ parking_traker_website/tracker/views.py
@@ -5,8 +5,20 @@ from .refresh import *
 # Create your views here.
 def main_view(request):
     if request.method == 'POST' and request.is_ajax():
-        print("ajax went through is called")
-        new_image()
-        return JsonResponse({})
-    return render(request, 'main.html',{})
+        print("ajax is called")
+        index =  int(request.POST.get('index'))
+     
+        cur = get_count(index)
+        return JsonResponse({'count':cur})
+    
+    
+    total = 12
+    current = -1
+    available = -1
+    context = {
+        'total': total,
+        'current': current,
+        'available': available        
+    }
+    return render(request, 'main.html',context)
 
